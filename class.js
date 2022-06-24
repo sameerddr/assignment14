ac70f8c5d89df34e269a91ed894d2dc5f70e36b1
Sameer <sameer.writes.70@gmail.com>
2022-06-24 16:42:01 +0530

    assignment14

---
+++ class.js
@@ -0,0 +1,27 @@
+// Q1-- >
+// var i;
+// var n;
+
+// function printnumber(i, n) {
+//     if (i == n) {
+//         return;
+//     }
+//     console.log(i);
+//     printnumber(i + 1, n);
+// }
+
+// printnumber(1, 5);
+
+// Q2-->
+// var i;
+// var n;
+
+// function printnumber(i, n) {
+//     if (i == n) {
+//         return;
+//     }
+//     printnumber(i + 1, n);
+//     console.log(i);
+// }
+
+// printnumber(1, 5);

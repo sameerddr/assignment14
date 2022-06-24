c15723fff6ff9684cd9f63004edd8aca2c868ba9
Sameer <sameer.writes.70@gmail.com>
2022-06-24 18:10:32 +0530

    all questions

--- sameer.js
+++ sameer.js
@@ -59,14 +59,70 @@
 // fibo(0, 1, 1);
 
 // Q6-->
-function printnumber(i, n, sum) {
-    if (i == n) {
-        sum += i;
-        console.log(sum);
-        return;
-    }
-    sum += i;
-    printnumber(i + 1, n, sum);
-}
+// function printnumber(i, n, sum) {
+//     if (i == n) {
+//         sum += i;
+//         console.log(sum);
+//         return;
+//     }
+//     sum += i;
+//     printnumber(i + 1, n, sum);
+// }
 
-printnumber(1, 5, 0);
+// printnumber(1, 5, 0);
+
+// Q7-->
+// function product(i, n, sum) {
+//     if (i == n) {
+//         sum *= i;
+//         console.log(sum);
+//         return;
+//     }
+//     sum *= i;
+//     product(i + 1, n, sum);
+// }
+
+// product(1, 5, 1);
+
+// Q8-->
+// function isPrime(n, i) {
+//     // Base cases
+//     if (n <= 2)
+//         return (n == 2) ? true : false;
+//     if (n % i == 0)
+//         return false;
+//     if (i * i > n)
+//         return true;
+//     // Check for next divisor
+//     return isPrime(n, i + 1);
+// }
+// let n = 12;
+// if (isPrime(n, 2))
+//     console.log("Yes it's a Prime");
+// else
+//     console.log(" it's not a Prime");
+
+// Q9-->
+// function num(num1, num2, sum) {
+//     if (num1 <= num2) {
+//         if (num1 % 2 == 0) {
+//             sum += num1;
+//             console.log(num1);
+//             console.log(sum);
+//         }
+//         return num(num1 + 1, num2, 0);
+//     }
+// }
+// console.log(num(20, 30, 0));
+
+// Q10-->
+
+function pow(x, y) {
+    let power = 1,
+        i;
+    for (let i = 1; i < y; i++) {
+        power = x ** y;
+    }
+    return console.log(power);
+}
+pow(2, 3);
